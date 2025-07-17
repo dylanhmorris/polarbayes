91a639d3e2822cdcb3993092a93f2310755526ce
Dylan H. Morris <dylan@dylanhmorris.com>
2025-07-17 09:34:00 -0400

    Split spread and gather

---
+++ polarbayes/gather.py
@@ -0,0 +1,54 @@
+def gather_variables(data: pl.DataFrame,
+                     index_cols = [],
+                     exclude = ["chain", "draw"],
+                     value_name = "value",
+                     variable_name = "variable"):
+    """
+    """
+    index = sorted(set(index_cols).union(set(exclude)))
+    
+    # more informative error message than `unpivot()` gives on its own
+    [_assert_not_in_index_columns(k, v, index) for k, v in 
+     dict(value_name=value_name, variable_name=variable_name).items()]
+    
+    return data.unpivot(
+        index = index,
+        variable_name = variable_name,
+        value_name = value_name).select(
+        pl.col("chain"), pl.col("draw"), pl.all().exclude(["chain", "draw"]) 
+        # always start the df with chain, then draw, then any other index cols 
+        # in alphabetical order
+    )
+
+def gather_draws(data: az.InferenceData,
+                 group: str = "posterior",
+                 combined: bool = True, 
+                 var_names: Iterable[str] = None, 
+                 filter_vars: str = None, 
+                 num_samples: int = None, 
+                 rng: bool | int | np.random.Generator = None) -> pl.DataFrame:
+    # need to extract all variables jointly to ensure same 
+    # draws for each
+    extracted = az.extract(
+            data,
+            group=group,
+            combined=combined,
+            var_names=var_names,
+            filter_vars=filter_vars,
+            num_samples=num_samples,
+            keep_dataset=True,
+            rng=rng)
+    var_names = extracted.data_vars.keys()
+    return pl.concat([
+        gather_variables(*spread_draws_and_get_index_cols(
+            extracted,
+            group=group,
+            var_names=var,
+            combined=False,
+            filter_vars=None,
+            num_samples=None,
+            rng=False,
+            enforce_drop_chain_draw=combined))
+        for var in var_names
+    ],
+                     how="diagonal")
--- polarbayes/spread.py
+++ polarbayes/spread.py
@@ -1,94 +1,165 @@
+import arviz as az
 import numpy as np
+import pandas as pd
 import polars as pl
 from typing import Iterable
 
 
-def spread_draws(
-    posteriors: dict[str, np.ndarray], variable_names: Iterable[str]
-) -> pl.DataFrame:
+def spread_draws_to_pandas_(
+    data: az.InferenceData,
+    group: str = "posterior",
+    combined: bool = True,
+    var_names: Iterable[str] = None,
+    filter_vars: str = None,
+    num_samples: int = None,
+    rng: bool | int | np.random.Generator = None,
+) -> pd.DataFrame:
     """
-    Given a dictionary of posteriors,
-    return a long-form polars dataframe
-    indexed by draw, with variable
-    values (equivalent of tidybayes
-    spread_draws() function).
+    Convert an ArviZ InferenceData object group to a Pandas
+    DataFrame of tidy (spread) draws, using the syntax of
+    arviz.extract
 
     Parameters
     ----------
-    posteriors
-        a dictionary of posteriors
-        with variable names as keys and numpy ndarrays
-        as values (with the first axis corresponding
-        to the posterior draw number.
+    data
+        Data to convert.
 
-    variable_names
-        list of strings or of tuples identifying which variables to
-        retrieve.
+    group
+        `group` parameter passed to `az.extract`.
+
+    combined
+        `combined` parameter passed to `az.extract`.
+
+    var_names
+        `var_names` parameter passed to `az.extract`.
+
+    filter_vars
+        `var_names` parameter passed to `az.extract`.
+
+    num_samples
+        `num_samples` parameter passed to `az.extract`.
+
+    rng
+        `rng` parameter passed to `az.extract`.
 
     Returns
     -------
-    pl.DataFrame
-        Polars dataframe with the spread draws
+    pd.DataFrame
+       Pandas DataFrame with a MultiIndex on the chain id,
+       draw id, and any additional indices determined by
+       the dimensions of the variables selected via
+       `var_names` or `filter_vars`.
     """
-
-    for i_var, v in enumerate(variable_names):
-        if isinstance(v, str):
-            v_dims = None
-        else:
-            v_dims = v[1:]
-            v = v[0]
-
-        post = posteriors.get(v)
-        long_post = post.flatten()[..., np.newaxis]
-
-        indices = np.array(list(np.ndindex(post.shape)))
-        n_dims = indices.shape[1] - 1
-        if v_dims is None:
-            dim_names = [
-                ("{}_dim_{}_index".format(v, k), pl.Int64) for k in range(n_dims)
-            ]
-        elif len(v_dims) != n_dims:
-            raise ValueError(
-                "incorrect number of dimension names provided for variable {}".format(v)
-            )
-        else:
-            dim_names = [(v_dim, pl.Int64) for v_dim in v_dims]
-
-        p_df = pl.DataFrame(
-            np.concatenate([indices, long_post], axis=1),
-            schema=([("draw", pl.Int64)] + dim_names + [(v, pl.Float64)]),
-        )
-
-        if i_var == 0:
-            df = p_df
-        else:
-            df = df.join(p_df, on=[col for col in df.columns if col in p_df.columns])
-        pass
-
+    df = az.extract(
+        data,
+        group=group,
+        combined=combined,
+        var_names=var_names,
+        filter_vars=filter_vars,
+        num_samples=num_samples,
+        keep_dataset=True,
+        rng=rng,
+    ).to_dataframe()
+    if combined:
+        # az.extract with combined=True assumes that the InferenceData object has
+        # "chain" and "draw" named dimensions and errors otherwise.
+        # When the resultant dataset is converted to a pandas dataframe via .to_dataframe(),
+        # "chain" and "draw" are included _both_ as column names and as Pandas MultiIndex
+        # names. We only want/need them in the MultiIndex, so we drop the columns, which
+        # are guaranteed to exist per by the az.extract assumption mentioned above.
+        df = df.drop(["chain", "draw"], axis=1)
     return df
 
 
-def spread_and_recover_ids(
-    posteriors: dict[str, np.ndarray],
-    variable_names: Iterable[str],
-    id_mappers: dict = {},
-    id_datatype: str = "str",
-    keep_internal: bool = False,
+def spread_draws_and_get_index_cols(
+    data: az.InferenceData,
+    group: str = "posterior",
+    combined: bool = True,
+    var_names: Iterable[str] = None,
+    filter_vars: str = None,
+    num_samples: int = None,
+    rng: bool | int | np.random.Generator = None,
+    enforce_drop_chain_draw: bool = False,
+) -> tuple[pl.DataFrame, tuple]:
+    """
+    Convert an ArviZ InferenceData object to a polars
+    DataFrame of tidy (spread) draws, using the syntax of
+    arviz.extract. Return that DataFrame alongside a tuple
+    giving the names of the DataFrame's index columns.
+
+    Parameters
+    ----------
+    data
+        Data to convert.
+
+    group
+        `group` parameter passed to `az.extract`.
+
+    combined
+        `combined` parameter passed to `az.extract`.
+
+    var_names
+        `var_names` parameter passed to `az.extract`.
+
+    filter_vars
+        `var_names` parameter passed to `az.extract`.
+
+    num_samples
+        `num_samples` parameter passed to `az.extract`.
+
+    rng
+        `rng` parameter passed to `az.extract`.
+
+    Returns
+    -------
+    tuple[pl.DataFrame, tuple]
+        Two-entry whose first entry is the DataFrame and whose
+        second entry is a tuple giving the names of the DataFrame's
+        index columns, typically `"chain"`, "draw"`, and additional
+        columns determined by the dimensions of the variables chosen
+        via `var_names` or `filter_vars`.
+    """
+
+    df = spread_draws_to_pandas_(
+        data,
+        group=group,
+        combined=combined,
+        var_names=var_names,
+        filter_vars=filter_vars,
+        num_samples=num_samples,
+        rng=rng,
+    )
+    if enforce_drop_chain_draw:
+        df = df.drop(["chain", "draw"], axis=1)
+    return (pl.DataFrame(df.reset_index()), tuple(df.index.names))
+
+
+def _assert_not_in_index_columns(arg_name, arg_value, index_columns) -> None:
+    if arg_value in index_columns:
+        raise ValueError(
+            f"Specified {arg_name}='{arg_value}' for the output data frame "
+            f"but there is an index column named '{arg_value}' "
+            f"in the input data frame. Either specify a different {arg_name} "
+            f"or rename the index column named '{arg_value}'."
+        )
+
+
+def spread_draws(
+    data: az.InferenceData,
+    group: str = "posterior",
+    combined: bool = True,
+    var_names: Iterable[str] = None,
+    filter_vars: str = None,
+    num_samples: int = None,
+    rng: bool | int | np.random.Generator = None,
 ) -> pl.DataFrame:
-    temp_spread = spread_draws(posteriors, variable_names)
-
-    new_cols = []
-
-    for dim_name, mapper in id_mappers.items():
-        if dim_name in temp_spread.columns:
-            map_vals = temp_spread.get_column(dim_name).to_numpy()
-            new_cols.append(
-                pl.lit(mapper[map_vals].astype(id_datatype)).alias(dim_name)
-            )
-
-            if keep_internal:
-                new_cols.append(
-                    temp_spread.get_column(dim_name).alias(dim_name + "_internal")
-                )
-
-    return temp_spread.with_columns(new_cols)
+    result, _ = spread_draws_and_get_index_cols(
+        data,
+        group=group,
+        combined=combined,
+        var_names=var_names,
+        filter_vars=filter_vars,
+        num_samples=num_samples,
+        rng=rng,
+    )
+    return result
