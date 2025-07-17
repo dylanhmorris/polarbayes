53364eb5cdd48f58accb9dd4d9ab55d3d2dd7644
Dylan H. Morris <dylan@dylanhmorris.com>
2025-07-17 17:40:04 -0400

    Make gather more directly mirror tidybayes::gather

--- polarbayes/gather.py
+++ polarbayes/gather.py
@@ -1,13 +1,41 @@
+from collections.abc import Sequence
 from typing import Iterable
 
 import arviz as az
 import numpy as np
 import polars as pl
+import polars.selectors as cs
+from polars._typing import ColumnNameOrSelector
 
 from polarbayes.spread import spread_draws_and_get_index_cols
 
 
-def _assert_not_in_index_columns(arg_name, arg_value, index_columns) -> None:
+def _assert_not_in_index_columns(
+    arg_name: str, arg_value: str, index_columns: Iterable[str]
+) -> None:
+    """
+    Assert that a specified value is not present in a set of index columns,
+    with an informative error message.
+
+    Parameters
+    ----------
+    arg_name
+        The name of the argument being validated.
+    arg_value
+        The value of the argument to check against index columns.
+    index_columns
+        Iterable of index column names to check against.
+
+    Returns
+    -------
+    None
+       If validation passes.
+
+    Raises
+    ------
+    ValueError
+        If `arg_value` is found in `index_columns`.
+    """
     if arg_value in index_columns:
         raise ValueError(
             f"Specified {arg_name}='{arg_value}' for the output data frame "
@@ -15,21 +43,55 @@ def _assert_not_in_index_columns(arg_name, arg_value, index_columns) -> None:
             f"in the input data frame. Either specify a different "
             f" {arg_name} or rename the index column named '{arg_value}'."
         )
+    return None
 
 
 def gather_variables(
-    data: pl.DataFrame,
-    index_cols=[],
-    exclude=["chain", "draw"],
-    value_name="value",
-    variable_name="variable",
+    data: pl.LazyFrame | pl.DataFrame,
+    index: ColumnNameOrSelector | Sequence[ColumnNameOrSelector] | None = None,
+    value_name: str = "value",
+    variable_name: str = "variable",
 ):
-    """ """
-    index = sorted(set(index_cols).union(set(exclude)))
+    """
+    Gather variable columns into key-value pairs.
+    Light wrapper of `pl.DataFrame.unpivot()`.
+    designed for use with `spread_draws()` output.
+
+    Parameters
+    ----------
+    data
+        Input DataFrame to (un)pivot from wide to long format.
+    index
+        Polars expression selecting mandatory or optional columns to
+        index the gather. Passed as the `index` argument to
+        `pl.DataFrame.unpivot()`. If `None` (default), use the columns
+        `["chain", "draw"]` if they are present. Those are the MCMC
+        index columns created when `spread_draws()` is on a standard
+        `az.InferenceData` object.
+    value_name
+        Name for the value column in the output DataFrame. Default `"value"`.
+    variable_name
+        Name for the variable column in the output DataFrame. Default `"variable"`.
+
+    Returns
+    -------
+    pl.LazyFrame | pl.DataFrame
+        Unpivoted (pivoted longer) tidy data frame with index columns plus
+        variable name and value columns.
+
+    Raises
+    ------
+    ValueError
+        If `value_name` or `variable_name` conflicts with requested index columns.
+    """
+    if index is None:
+        index = cs.by_name("chain", "draw", require_all=False)
+
+    index_names = data.select(index).collect_schema().names()
 
     # more informative error message than `unpivot()` gives on its own
     [
-        _assert_not_in_index_columns(k, v, index)
+        _assert_not_in_index_columns(k, v, index_names)
         for k, v in dict(
             value_name=value_name, variable_name=variable_name
         ).items()
@@ -37,11 +99,6 @@ def gather_variables(
 
     return data.unpivot(
         index=index, variable_name=variable_name, value_name=value_name
-    ).select(
-        pl.col(exclude),
-        pl.all().exclude(exclude),
-        # always start the df with the "exclude" columns (default "chain"
-        # and "draw"), then any other index cols in alphabetical order
     )
 
 
