aeb9b51d5d8946e973adf72a48bb4e1ca0fbc6a2
Dylan H. Morris <dylanhmorris@users.noreply.github.com>
2025-07-22 18:28:53 -0400

    Update __init__.py to correct imported function names (#10)

--- polarbayes/__init__.py
+++ polarbayes/__init__.py
@@ -1,3 +1,9 @@
-from polarbayes.spread import spread_and_recover_ids, spread_draws
+from polarbayes.gather import gather_draws, gather_variables
+from polarbayes.spread import spread_draws, spread_draws_and_get_index_cols
 
-__all__ = ["spread_draws", "spread_and_recover_ids"]
+__all__ = [
+    "spread_draws",
+    "spread_draws_and_get_index_cols",
+    "gather_variables",
+    "gather_draws",
+]
